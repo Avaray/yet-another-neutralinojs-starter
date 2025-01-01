a90247742fbaf2fb1352486f4cafa7e67d32ee37
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-01 20:37:05 +0100

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,21 +1,28 @@
-import { app, events, init, window as W } from "@neutralinojs/lib"
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import 'virtual:uno.css'
+import { app, events, init, window as W } from "@neutralinojs/lib";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+// import "virtual:uno.css";
 
-try { init() } catch (err) { console.warn('Neutralino.js failed to initialize.', err) }
+import "@unocss/reset/tailwind.css";
+import "uno.css";
 
-const rootElement = document.getElementById('root');
+try {
+  init();
+} catch (err) {
+  console.warn("Neutralino.js failed to initialize.", err);
+}
+
+const rootElement = document.getElementById("root");
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <StrictMode>
       <App />
     </StrictMode>,
-  )
+  );
 }
 
-events.on('windowClose', () => app.exit())
+events.on("windowClose", () => app.exit());
 
-W.focus()
+W.focus();
