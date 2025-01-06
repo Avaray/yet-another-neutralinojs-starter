4b1597a4b6e880de1bf8e5b046d63078e92a077d
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-06 22:46:01 +0100

    Update Info.tsx

--- web/src/pages/Info.tsx
+++ web/src/pages/Info.tsx
@@ -4,11 +4,19 @@ import { Icon } from "@iconify/react";
 export default function Info() {
   return (
     <>
-        <p>Operating System: <span className='font-bold'>{NL_OS} {NL_ARCH}</span></p>
-        <p>Framework version: <span className='font-bold'>{NL_VERSION}</span></p>
-        <p>Client version: <span className='font-bold'>{NL_CVERSION}</span></p>
-        <p>Mode: <span className='capitalize font-bold'>{NL_MODE}</span></p>
       <div className="flex flex-grow p-3 m-3 flex-col whitespace-nowrap">
+        <p>
+          Operating System: <span className="font-bold">{NL_OS} {NL_ARCH}</span>
+        </p>
+        <p>
+          Framework version: <span className="font-bold">{NL_VERSION}</span>
+        </p>
+        <p>
+          Client version: <span className="font-bold">{NL_CVERSION}</span>
+        </p>
+        <p>
+          Mode: <span className="capitalize font-bold">{NL_MODE}</span>
+        </p>
       </div>
       <div className="flex-grow p-4">
         <Icon
