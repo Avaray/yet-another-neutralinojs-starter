86692f3419e81a131311be2ed527bb1d239c269c
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-07 16:44:47 +0100

    Create i18next.ts

---
+++ web/src/i18next.ts
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import resourcesToBackend from "i18next-resources-to-backend";
+
+i18n
+  .use(initReactI18next)
+  .use(
+    resourcesToBackend((language: string) =>
+      import(`./locales/${language}.json`)
+    ),
+  )
+  .init({
+    lng: "pl",
+    fallbackLng: "en",
+    ns: ["translation"],
+    defaultNS: "translation",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
