f5dd72cd714064ba6ba7e043c70b91d5b7329c19
Avaray <1459553+Avaray@users.noreply.github.com>
2025-11-15 12:04:18 +0100

    Update Info.tsx

--- web/src/pages/Info.tsx
+++ web/src/pages/Info.tsx
@@ -1,22 +1,38 @@
 import { Icon } from "@iconify/react";
 // import { app, events, os, computer } from "@neutralinojs/lib";
+import { useTranslation } from "react-i18next";
 
 export default function Info() {
+  const { t } = useTranslation();
+
+  const modeElement = (mode: string) => {
+    const url = `https://neutralino.js.org/docs/configuration/modes#${mode}`;
+    const paragraph = (
+      <p className="flex flex-row">
+        {t("mode")}:
+        <a href={url} target="_blank" rel="noreferrer">
+          <span className="capitalize font-bold">{mode}</span>
+          <Icon icon="arcticons:url-checker" className="h-7 w-7" />
+        </a>
+      </p>
+    );
+    return paragraph;
+  };
+
   return (
     <>
       <div className="flex flex-grow p-3 m-3 flex-col whitespace-nowrap">
         <p>
-          Operating System: <span className="font-bold">{NL_OS} {NL_ARCH}</span>
+          {t("os")}: <span className="font-bold">{NL_OS} {NL_ARCH}</span>
         </p>
         <p>
-          Framework version: <span className="font-bold">{NL_VERSION}</span>
+          {t("frameworkVersion")}:{" "}
+          <span className="font-bold">{NL_VERSION}</span>
         </p>
         <p>
-          Client version: <span className="font-bold">{NL_CVERSION}</span>
+          {t("clientVersion")}: <span className="font-bold">{NL_CVERSION}</span>
         </p>
-        <p>
-          Mode: <span className="capitalize font-bold">{NL_MODE}</span>
-        </p>
+        {modeElement(NL_MODE)}
       </div>
       <div className="flex-grow p-4">
         <Icon
